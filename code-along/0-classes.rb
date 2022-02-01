866e95b1da4360468b2be2b83af0a1aba8ad9319
dwilches13 <daniel.wilches@kellogg.northwestern.edu>
2022-02-01 16:39:24 +0000

    WIP

--- code-along/0-classes.rb
+++ code-along/0-classes.rb
@@ -3,6 +3,35 @@
 # should be the top-level directory (i.e. /workspace/your-app/) and then run:
 # rails runner code-along/0-classes.rb
 
+brians_favorite_food = "tacos"
+bens_favorite_food = "sushi"
+puts brians_favorite_food.upcase
+puts bens_favorite_food.upcase
+
+# numer_of_tacos = 5
+# puts numer_of_tacos.class
+
 # String Class
 
 # String Methods
+class Dog
+
+    def speak
+        puts "Woof"
+    end
+
+end
+
+class Cat
+
+    def speak
+        puts "Meow"
+    end
+
+end
+
+jenkins = Dog.new
+jenkins.speak
+
+rover = Dog.new
+rover.speak
--- labs/1-models.rb
+++ labs/1-models.rb
@@ -12,8 +12,30 @@ Contact.destroy_all
 # 1b. check out the model file
 
 # 2. create 1-2 new contacts for each company (they can be made up)
+apple = Company.where({name: "Apple, Inc."})[0]
+apple_id = apple.id
+
+values = {first_name: "Stiven", last_name: "Vega", email: "vega@apple.com", phone_number: "28", company_id: apple_id}
+contact = Contact.new(values)
+contact.save
+
+amazon = Company.where({name: "Amazon.com, Inc."})[0]
+amazon_id = amazon.id
+
+values = {first_name: "Andres", last_name: "Llinas", email: "llinas@amazon.com", phone_number: "26", company_id: amazon_id}
+contact = Contact.new(values)
+contact.save
+
+contact = Contact.new
+contact.first_name = "Andres"
+contact.last_name = "Roman"
+contact.email = "roman@tesla.com"
+contact.phone_number = "6"
+contact.company_id = amazon_id
+contact.save
 
 # 3. write code to display how many contacts are in the database AND each contact's info (name, email), e.g.:
+# puts vega.read_attribute(first_name)
 
 # ---------------------------------
 # Contacts: 4
@@ -21,3 +43,9 @@ Contact.destroy_all
 # Craig Federighi - craig@apple.com
 # Elon Musk - elon@tesla.com
 # Tim Cook - tim@apple.com
+
+puts "There are now #{Contact.all.count} contacts."
+all_contacts = Contact.all
+for contact in all_contacts
+    puts "#{contact.first_name} #{contact.last_name} - #{contact.email}"
+end
